2694069c09707df2d7dd9f3d382daa2ffdd80c16
Carlos Hernandez <chernandezmiranda@gmail.com>
2024-01-14 16:12:34 -0800

    Completed assignment

--- src/store.js
+++ src/store.js
@@ -1,6 +1,8 @@
+//folder src file store.js
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './features/counterSlice'
 import todoReducer from './features/todoSlice'
+
 
 export const store = configureStore({
     reducer: {
